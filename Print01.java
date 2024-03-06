f0cee91226ad937c61409d8332f62893224ec471
wook89 <162427559+wook89@users.noreply.github.com>
2024-03-06 17:46:36 +0900

    Add files via upload

---
+++ Print01.java
@@ -0,0 +1,17 @@
+package sec04;
+
+public class Print01 {
+	public static void main(String[] args) {
+		String myName = "홍길동";
+		int myAge = 20;
+		
+		System.out.println("이름 : 홍길동");
+		System.out.println("나이 : 20");
+		System.out.println();
+		System.out.println();
+		System.out.print("이름 : 홍길동");
+	    System.out.print("  ");
+		System.out.println("나이 : 20");
+	}
+
+}
